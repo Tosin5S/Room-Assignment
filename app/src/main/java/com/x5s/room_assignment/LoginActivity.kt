21b6900b653578aee98fedc1dd52d9a75278a490
5s <olyguy.aq@gmail.com>
2021-05-29 14:26:49 +0100

    Created LoginActivity

--- app/src/main/java/com/x5s/room_assignment/LoginActivity.kt
+++ app/src/main/java/com/x5s/room_assignment/LoginActivity.kt
@@ -1,6 +1,5 @@
 package com.x5s.room_assignment
 
-
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
@@ -11,43 +10,43 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     private var userViewModel: UserViewModel? = null
     private var usernameText: EditText? = null
     private var passwordText: EditText? = null
-    private var signUpButton: Button? = null
-    //private var loginButton: Button? = null
+    //private var signUpButton: Button? = null
+    private var loginButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         usernameText = findViewById<View>(R.id.username) as EditText
         passwordText = findViewById<View>(R.id.password) as EditText
-        signUpButton = findViewById<View>(R.id.signup) as Button
-        //loginButton = findViewById<View>(R.id.login) as Button
+        //signUpButton = findViewById<View>(R.id.signup) as Button
+        loginButton = findViewById<View>(R.id.login) as Button
 
         userViewModel = ViewModelProviders.of(this, UserViewModel.Factory(applicationContext)).get(UserViewModel::class.java)
 
-        //val signup = getSharedPreferences("PREFERENCE", Context.MODE_PRIVATE)
-          //      .getBoolean("signup", true)
+        //val login = getSharedPreferences("PREFERENCE", Context.MODE_PRIVATE)
+          //  .getBoolean("signup", true)
 
-        //if (signup) {
-            signUpButton!!.visibility = View.VISIBLE
-          //  loginButton!!.visibility = View.GONE
-        //} else {
-         //   signUpButton!!.visibility = View.GONE
-            //loginButton!!.visibility = View.VISIBLE
+        //if (login) {
+            //signUpButton!!.visibility = View.VISIBLE
+         //   loginButton!!.visibility = View.GONE
+       // } else {
+            //signUpButton!!.visibility = View.GONE
+            loginButton!!.visibility = View.VISIBLE
 
        // }
-        signUpButton!!.setOnClickListener {
+        /*signUpButton!!.setOnClickListener {
             userViewModel!!.createUser(usernameText!!.text.toString(), passwordText!!.text.toString())
             getSharedPreferences("PREFERENCE", Context.MODE_PRIVATE).edit()
-                    .putBoolean("signup", false).apply()
+                .putBoolean("signup", false).apply()
             Toast.makeText(baseContext, "Successfully Created An Account!", Toast.LENGTH_LONG).show()
-        }
+        }*/
 
-        /*loginButton!!.setOnClickListener {
+        loginButton!!.setOnClickListener {
             val isValid = userViewModel!!.checkValidLogin(usernameText!!.text.toString(), passwordText!!.text.toString())
             if (isValid) {
                 Toast.makeText(baseContext, "Successfully Logged In!", Toast.LENGTH_LONG).show()
@@ -56,8 +55,7 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(baseContext, "Invalid Login!", Toast.LENGTH_SHORT).show()
                 Log.i("Unsuccessful_Login", "Login was not successful")
             }
-        }*/
+        }
 
     }
-
 }
